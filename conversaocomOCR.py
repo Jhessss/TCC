e9c3674eb887af5f11afcccb332be3c232e567f0
Jhéssica Isabel Coelho <beelcs@hotmail.com>
2025-07-31 00:11:39 -0300

    extração OCR melhorada e adaptada ao tamanho do pdf

--- conversaocomOCR.py
+++ conversaocomOCR.py
@@ -1,25 +1,27 @@
 import pdfplumber
 import pytesseract
-from PIL import Image, ImageDraw
-
+from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-def extrair_texto_ocr_com_risco(pdf_path, page_number=0):
+def extrair_texto_ocr_completo(pdf_path):
+    texto_completo = ""
     with pdfplumber.open(pdf_path) as pdf:
-        page = pdf.pages[page_number]
-        im = page.to_image(resolution=300)
-        pil_image = im.original
+        for page in pdf.pages:
+            im = page.to_image(resolution=300)
+            pil_image = im.original
 
-        
-        gray = pil_image.convert("L")
-        texto = pytesseract.image_to_string(gray, lang='por')
+            gray = pil_image.convert("L")
+            threshold = 150
+            binarized = gray.point(lambda p: 255 if p > threshold else 0)
 
-        return texto
+            texto = pytesseract.image_to_string(binarized, lang='por', config='--psm 6')
+            texto_completo += texto + "\n\n"  # separa páginas com linhas em branco
 
+    return texto_completo
 
-pdf_path = "Anatel - Ato nº 2436, de 7 de março de 2023.pdf"
-texto = extrair_texto_ocr_com_risco(pdf_path, page_number=0)
+pdf_path = "L10973.pdf"
+texto = extrair_texto_ocr_completo(pdf_path)
 
 with open("ocr_extraido.txt", "w", encoding="utf-8") as f:
     f.write(texto)
